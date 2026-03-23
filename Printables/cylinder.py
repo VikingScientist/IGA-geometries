d897048dc2b2e71e5df45848ab20a1e2125fa585
Kjetil Andre Johannessen <kjetijo@gmail.com>
2026-03-23 13:44:23 +0100

    Added bulging layers (cylinder case)

--- Printables/cylinder.py
+++ Printables/cylinder.py
@@ -3,6 +3,7 @@ from splipy.io import G2, STL
 import splipy.curve_factory as cf
 import splipy.surface_factory as sf
 import splipy.volume_factory as vf
+from splipy.utils import rotation_matrix
 import numpy as np
 import re
 import click
@@ -16,10 +17,11 @@ import click
 @click.option('--print-width',           default=.02,      help='print width [m]')
 @click.option('--print-length',          default=1.8,      help='print length = 2pi*radius [m]')
 @click.option('--flat-floor',            default=True,     help='Flatten the bottom layer (degenerate face on patch 0)')
+@click.option('--bulge-layers',          default=0,        help='Bulge each layer to get 3d-printed texture, ranging from 0 [%] (no bulge) to 100 [%] (degenerate elements)')
 @click.option('--filename',              default='out.g2', help='Output GoTools (g2) filename')
 @click.option('--stl',                   is_flag=True,     help='Create a stl output file')
 
-def cylinder(h, p, layers, layer_height, print_width, print_length, flat_floor, filename, stl):
+def cylinder(h, p, layers, layer_height, print_width, print_length, flat_floor, bulge_layers, filename, stl):
     """ Creates a cylinder wall piece """
     nx = print_length / h # total number of length elements
     ny = print_width  / h # total number of width elements
@@ -42,6 +44,21 @@ def cylinder(h, p, layers, layer_height, print_width, print_length, flat_floor, 
     helix = helix.rebuild(p=p+1, n=nx+p+1)
 
     crossection = sf.square(size=(print_width, layer_height), lower_left=(radius,0)).rotate(np.pi/2, (1,0,0))
+
+    assert 0 <= bulge_layers <= 100, 'Bulge layers need to be between 0% and 100%'
+    if not bulge_layers == 0:
+        crossection = Surface()
+        crossection.raise_order(0,2)
+        t = np.pi/2/100 * bulge_layers
+        s = 1/3
+        crossection[0,1,:] = [ -s*np.sin(t),   s*np.cos(t)]
+        crossection[0,2,:] = [ -s*np.sin(t), 1-s*np.cos(t)]
+        crossection[1,1,:] = [1+s*np.sin(t),   s*np.cos(t)]
+        crossection[1,2,:] = [1+s*np.sin(t), 1-s*np.cos(t)]
+        crossection *= (print_width, layer_height)
+        crossection.rotate(np.pi/2, (1,0,0))
+        crossection += (radius,0,0)
+
     one_layer = vf.revolve(crossection)
     one_layer.swap('w','v')
     one_layer.swap('u','v')
