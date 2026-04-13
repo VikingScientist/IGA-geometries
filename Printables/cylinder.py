b3f2354eb540eb535e193c84bcc904fa7a004f72
Kjetil Andre Johannessen <kjetijo@gmail.com>
2026-04-13 11:51:42 +0200

    Printable/Cylinder: Swapped h parameter with nx,ny,nz

--- Printables/cylinder.py
+++ Printables/cylinder.py
@@ -10,7 +10,9 @@ import click
 
  
 @click.command()
-@click.option('--h',                     default=.02,      help='Element size in all directions')
+@click.option('--nx',                    default=20,       help='Number of elements in lengthwise direction')
+@click.option('--ny',                    default=1,        help='Number of elements in width direction')
+@click.option('--nz',                    default=1,        help='Number of elements in height direction')
 @click.option('--p',                     default=3,        help='The polynomial degree')
 @click.option('--layers',                default=7,        help='number of printable layers')
 @click.option('--layer-height',          default=.02,      help='layer height [m]')
@@ -21,17 +23,17 @@ import click
 @click.option('--filename',              default='out.g2', help='Output GoTools (g2) filename')
 @click.option('--stl',                   is_flag=True,     help='Create a stl output file')
 
-def cylinder(h, p, layers, layer_height, print_width, print_length, flat_floor, bulge_layers, filename, stl):
+def cylinder(nx, ny, nz, p, layers, layer_height, print_width, print_length, flat_floor, bulge_layers, filename, stl):
     """ Creates a cylinder wall piece """
-    nx = print_length / h # total number of length elements
-    ny = print_width  / h # total number of width elements
-    nz = layer_height / h # number of height elements per layer
+    # nx = print_length / h # total number of length elements
+    # ny = print_width  / h # total number of width elements
+    # nz = layer_height / h # number of height elements per layer
 
-    assert abs(nz-round(nz)) < 1e-10, 'Element size (h) does not divide layer-height'
+    # assert abs(nz-round(nz)) < 1e-10, 'Element size (h) does not divide layer-height'
     # nz *= layers          # total number of height elements
-    nx = round(nx)
-    ny = round(ny)
-    nz = round(nz)
+    # nx = round(nx)
+    # ny = round(ny)
+    # nz = round(nz)
 
     radius = print_length / 2 / np.pi
     height = layer_height * layers
